360e8431f38f33cd7fb90a556292c2dcd8008506
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-09-09 00:28:52 +0200

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="anonLLM",
-    version="0.1.3",
+    version="0.1.5",
     author="FS Ndzomga",
     author_email="ndzomgafs@gmail.com",
     description="Anonymizes personally identifiable information for "
