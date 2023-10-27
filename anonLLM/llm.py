a3a54bd8c8cd8120963ea97237b945b237a9f6f3
Sully Sullenberger <sully@sadburger.com>
2023-10-27 14:06:24 -0700

    fix deanonymizer usage and return type when output_format is provided to llm.generate

--- anonLLM/llm.py
+++ anonLLM/llm.py
@@ -82,14 +82,20 @@ class OpenaiLanguageModel:
                 print(f"Error: {err}")
                 break
 
-        if n_completions == 1:
-            deanonymized_response = (self.deanonymizer.deanonymize(valid_responses[0], mappings)
-                                     if self.anonymize else valid_responses[0])
-            return deanonymized_response
+        def _deanonymize(response):
+            if output_format:
+                for key, value in response.items():
+                    response[key] = self.deanonymizer.deanonymize(value, mappings)
+                return output_format.model_validate(response)
+            else:
+                return self.deanonymizer.deanonymize(response, mappings)
 
-        deanonymized_responses = [(self.deanonymizer.deanonymize(res, mappings)
-                                  if self.anonymize else res)
-                                  for res in valid_responses[:n_completions]]
+        deanonymized_responses = [_deanonymize(res) if self.anonymize else res
+                                  for res in valid_responses]
+
+        if n_completions == 1:
+            # if generating a single completion, return it directly
+            return deanonymized_responses[0]
         return deanonymized_responses
 
     def _model_structure_repr(self, model: Type[BaseModel]) -> str:
